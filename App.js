52917471c91f69b3feb91dc6cb51d135a3bc62dc
maoui810 <60634367+maoui810@users.noreply.github.com>
2021-06-20 22:19:48 -0700

    first

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>#3</Text>
+      </View>
+    </View>
+  );
+}
